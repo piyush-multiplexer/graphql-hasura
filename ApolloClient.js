36f72162ad93567fee13e15dacfcad57d0fb1bda
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-10-12 10:14:02 +0530

    integrate apollo and graphql with vue as client

---
+++ ApolloClient.js
@@ -0,0 +1,5 @@
+import ApolloClient from "apollo-boost";
+
+const apolloClient = new ApolloClient({
+  uri: "https://URL//v1/graphql",
+});
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueApollo from "vue-apollo";
+import apolloClient from "../ApolloClient";
 
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 new Vue({
   router,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
